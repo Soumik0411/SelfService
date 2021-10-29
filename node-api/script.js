ed6d87fd413061e3308074682d1bd5e04045b9af
neelabja321 <82468018+neelabja321@users.noreply.github.com>
2021-10-29 17:10:58 +0530

    UPDATED API

--- node-api/script.js
+++ node-api/script.js
@@ -3,7 +3,7 @@ var express = require('express');
 var mysql = require('mysql');
 var app =express();
 var request=require("request");
- 
+
 
 var masterModel = require("./masterModel");
 var bodyParser = require('body-parser');
@@ -36,17 +36,20 @@ connection.connect(function(error) {
 
 app.post('/verifystudent/', function(req,resp){
 
-      masterModel.get_student(connection,req.body.id,function(err, result){  
+      masterModel.get_student(connection,req.body.id.value,function(err, result){  
           //console.log(result);
-          var responseData = {
-            requestUrl : req.originalUrl,
-            data:result,     
+        if((result.length)==1){
+            var responseData = {
+            status : 'success',
+            data:true,
+            message: 'Student Verified Successfully'   
           }
-        //   console.log(responseData);
-          resp.send(otp);
-          if((responseData.data.length)==1){
+        // console.log(responseData);
+        
+          //resp.send(otp);
+          //if((responseData.data.length)==1){
             var otp=( Math.floor(Math.random() * (9999 - 1000) + 1000));
-               masterModel.add_studentotp(connection,req.body.id,otp,function(err, resultOtp,resultStudent){
+               masterModel.add_studentotp(connection,req.body.id.value,otp,function(err, resultOtp,resultStudent){
 
                 // SEND SMS
                  var smsstring="http://support.riceindia.org/login/send_otp?mobile="+resultStudent[0].contactNo+"&otp="+resultOtp
@@ -60,7 +63,18 @@ app.post('/verifystudent/', function(req,resp){
                 });
                    
               });
-            }          
+
+              resp.send(JSON.stringify(responseData))
+            }
+          //} 
+          else{
+              var responseData = {
+                status : 'success',
+                data:false,
+                message: 'Student Not Veified'   
+              }
+              resp.send(JSON.stringify(responseData))
+            }         
           
         });       
 
@@ -72,23 +86,35 @@ app.post('/verify-otp',function(req,res){
 
 
 
-  masterModel.get_otp(connection,req.body.id,req.body.otp,function(err, result){  
+  masterModel.get_otp(connection,req.body.id.value,req.body.otp,function(err, result){  
 
     var responseData = {
       status : 'success',
-      data:true,     
+      data:true, 
+      message:'OTP Veified'    
     }
    
     if(result.length == 1){
       res.send(JSON.stringify(responseData))
+      masterModel.update_otp(connection,req.body.id.value,req.body.otp,function(err,result){
+        if(err){
+          console.log(error);
+        }else{
+          console.log(responseData);
+        }
+      })
+    }else{
+      var responseData={
+        status : 'success',
+        data:'false',
+        message:'OTP not matched'
+      }
+      res.send(JSON.stringify(responseData))
     }
 
-    // if(req.body.otp==logi.otp && req.body.studentid==resultStudent.studentid){
-    //     res.send("You has been successfully registered");
-    // }
-    // else{
-    //     res.render('otp',{msg : 'otp is incorrect'});
-    // }
+
+
+
   });
 })
 

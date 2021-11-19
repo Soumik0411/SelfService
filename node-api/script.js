471fd319b6992f333f82ad9b5a63a8a6c2c4bb0e
Swagatam550 <swagatamb1999@hotmail.com>
2021-11-19 16:32:25 +0530

    first

--- node-api/script.js
+++ node-api/script.js
@@ -1,113 +1,118 @@
 
 var express = require('express');
 var mysql = require('mysql');
-var app =express();
-var request=require("request");
+var app = express();
+var request = require("request");
+var cors = require('cors');
 
 
 var masterModel = require("./masterModel");
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password:'Neelabja@123',
-    database: 'ais_student_master',
-    insecureAuth : true,
+  host: 'localhost',
+  user: 'root',
+  password: 'swag@1512',
+  database: 'selfservice',
+  insecureAuth: true,
 });
 
 
 
 
-connection.connect(function(error) {
-    console.log(error);
-    if(error){
-        console.log('Error in connection');
-    }else{
-       
-    }
+connection.connect(function (error) {
+  console.log(error);
+  if (error) {
+    console.log('Error in connection');
+  } else {
+
+  }
 });
 
 
+// verifying student
 
+app.post('/verifystudent/', function (req, resp) {
 
-app.post('/verifystudent/', function(req,resp){
+  masterModel.get_student(connection, req.body.studentid, function (err, result) {
+    console.log(result);
+    if ((result.length) == 1) {
+      var responseData = {
+        status: 'success',
+        data: true,
+        message: 'Student Verified Successfully'
+      }
+       //console.log(responseData);
 
-      masterModel.get_student(connection,req.body.id.value,function(err, result){  
-          //console.log(result);
-        if((result.length)==1){
-            var responseData = {
-            status : 'success',
-            data:true,
-            message: 'Student Verified Successfully'   
+      //resp.send(otp);
+      //if((responseData.data.length)==1){
+      var otp = (Math.floor(Math.random() * (9999 - 1000) + 1000));
+      masterModel.add_studentotp(connection, req.body.studentid, otp, function (err, resultOtp, resultStudent) {
+
+        // SEND SMS
+        var smsstring = "http://support.riceindia.org/login/send_otp?mobile=" + resultStudent[0].contactNo + "&otp=" + resultOtp
+
+        request.get(smsstring, function (error, response, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(response);
           }
-        // console.log(responseData);
-        
-          //resp.send(otp);
-          //if((responseData.data.length)==1){
-            var otp=( Math.floor(Math.random() * (9999 - 1000) + 1000));
-               masterModel.add_studentotp(connection,req.body.id.value,otp,function(err, resultOtp,resultStudent){
+        });
 
-                // SEND SMS
-                 var smsstring="http://support.riceindia.org/login/send_otp?mobile="+resultStudent[0].contactNo+"&otp="+resultOtp
+      });
 
-                 request.get(smsstring,function(error,response,body){
-                    if(error){
-                          console.log(error);
-                    }else{
-                          console.log(response);
-                  }
-                });
-                   
-              });
+      resp.send(JSON.stringify(responseData))
+    }
+    //} 
+    else {
+      //console.log(result);
+      var responseData = {
+        status: 'failure',
+        data: false,
+        message: 'Student Not Veified'
+      }
+      resp.send(JSON.stringify(responseData))
+      
+    }
+    
 
-              resp.send(JSON.stringify(responseData))
-            }
-          //} 
-          else{
-              var responseData = {
-                status : 'success',
-                data:false,
-                message: 'Student Not Veified'   
-              }
-              resp.send(JSON.stringify(responseData))
-            }         
-          
-        });       
+  });
 
 })
 
 // verification of OTP
 
-app.post('/verify-otp',function(req,res){
+app.post('/verify-otp', function (req, res) {
 
 
 
-  masterModel.get_otp(connection,req.body.id.value,req.body.otp,function(err, result){  
+  masterModel.get_otp(connection, req.body.studentid, req.body.otp, function (err, result) {
 
     var responseData = {
-      status : 'success',
-      data:true, 
-      message:'OTP Veified'    
+      status: 'success',
+      data: true,
+      message: 'OTP Veified'
     }
-   
-    if(result.length == 1){
+
+    if (result.length == 1) {
       res.send(JSON.stringify(responseData))
-      masterModel.update_otp(connection,req.body.id.value,req.body.otp,function(err,result){
-        if(err){
+      masterModel.update_otp(connection, req.body.studentid, req.body.otp, function (err, result) {
+        if (err) {
           console.log(error);
-        }else{
+        } else {
           console.log(responseData);
         }
       })
-    }else{
-      var responseData={
-        status : 'success',
-        data:'false',
-        message:'OTP not matched'
+    } else {
+      var responseData = {
+        status: 'failure',
+        data: 'false',
+        message: 'OTP not matched'
       }
       res.send(JSON.stringify(responseData))
     }
@@ -116,6 +121,166 @@ app.post('/verify-otp',function(req,res){
 
 
   });
+
+  // masterModel.update_session(connection, req.body.otp, function (err, result) {
+
+  //   var responseData = {
+  //     status: 'success',
+  //     data: true,
+  //     message: 'SESSION EXPIRED'
+  //   }
+
+  //   if (result.length == 1) {
+  //     res.send(JSON.stringify(responseData))
+  //     masterModel.update_otp(connection, req.body.otp, function (err, result) {
+  //       if (err) {
+  //         console.log(error);
+  //       } else {
+  //         console.log(responseData);
+
+  //       }
+  //     })
+  //   } else {
+  //     var responseData = {
+  //       status: 'failure',
+  //       data: 'false',
+  //       message: 'OTP not matched'
+  //     }
+  //     res.send(JSON.stringify(responseData))
+  //   }
+
+
+
+
+  // });
+
 })
+
+app.post('/view_transaction_history/', function (req, resp) {
+
+  masterModel.get_transaction(connection, req.body.studentid, function (err, result) {
+    //console.log(result);
+    if (err){
+      console.log(err);
+    }
+    else{
+      var responseData = {
+      //requestUrl: req.originalUrl,
+      status: 'success',
+      data: result,
+    }
+    //   console.log(responseData);
+    resp.send(responseData);
+  }
+
+  });
+
+
+});
+
+app.post('/view_total_commitment/', function (req, resp) {
+
+  masterModel.get_commitment(connection, req.body.studentid, function (err, result) {
+    //console.log(result);
+    if (err){
+      console.log(err);
+    }
+    else{
+    var responseData = {
+      //requestUrl: req.originalUrl,
+      data: result,
+    }
+    //   console.log(responseData);
+    resp.send(responseData);
+  }
+
+  });
+
+  app.post('/view_total_paid_amount/', function (req, resp) {
+
+    masterModel.get_paidAmount(connection, req.body.studentid, function (err, result) {
+      //console.log(result);
+      if (err){
+        console.log(err);
+      }
+      else{
+      var responseData = {
+        //requestUrl: req.originalUrl,
+        data: result,
+      }
+      //   console.log(responseData);
+      resp.send(responseData);
+    }
+  
+    });
+
+    
+
+  
+
+
+});
+
+})
+
+app.post('/current_dues/', function (req, resp) {
+
+  masterModel.get_current_dues(connection, req.body.studentid, function (err, result) {
+    //console.log(result);
+    if (err){
+      console.log(err);
+    }
+    else{
+    var responseData = {
+      //requestUrl: req.originalUrl,
+      data: result,
+    }
+    //   console.log(responseData);
+    resp.send(responseData);
+  }
+
+  });
+});
+
+app.post('/upcoming_dues/', function (req, resp) {
+
+  masterModel.get_upcoming_dues(connection, req.body.studentid, function (err, result) {
+    //console.log(result);
+    if (err){
+      console.log(err);
+    }
+    else{
+    var responseData = {
+      //requestUrl: req.originalUrl,
+      data: result,
+    }
+    //   console.log(responseData);
+    resp.send(responseData);
+  }
+
+  });
+});
+
+app.post('/previous_dues/', function (req, resp) {
+
+  masterModel.get_previous_dues(connection, req.body.studentid, function (err, result) {
+    //console.log(result);
+    if (err){
+      console.log(err);
+    }
+    else{
+    var responseData = {
+      //requestUrl: req.originalUrl,
+      data: result,
+    }
+    //   console.log(responseData);
+    resp.send(responseData);
+  }
+
+  });
+});
+
+
+
 
 app.listen(4000);
